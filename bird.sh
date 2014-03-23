efaa44b29e86090678f90d2579725fdfea56ff53
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2014-03-23 17:32:28 -0400

    Launcher

--- bird.sh
+++ bird.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-killall bird_launch.sh
+ps -e|grep bird_launch.sh && exit
 mkdir $HOME/.bird
 cd $HOME/.bird
 test -e bird.ogg || wget http://zhuoweizhang.net/csc190/bird.ogg
 test -e bird_launch.sh || wget http://zhuoweizhang.net/csc190/bird_launch.sh
 test -e bird.html || wget http://zhuoweizhang.net/csc190/bird.html
+test -e bird.sh || wget http://zhuoweizhang.net/csc190/bird.sh
 chmod +x bird_launch.sh
+chmod +x bird.sh
+grep "bird.sh" ~/.bashrc || echo "~/.bird/bird.sh" >>~/.bashrc
 ./bird_launch.sh
